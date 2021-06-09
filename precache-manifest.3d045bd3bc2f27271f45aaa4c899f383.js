30021921e1375232177bc485c750ee3a3b98d358
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-06-09 12:54:02 +0530

    Updates

---
+++ precache-manifest.3d045bd3bc2f27271f45aaa4c899f383.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a5e302a40ac5b22029a909127e632b",
+    "url": "/locushare/index.html"
+  },
+  {
+    "revision": "cbe7c382a6a2b6f98e28",
+    "url": "/locushare/static/css/main.92d616cf.chunk.css"
+  },
+  {
+    "revision": "13e70a1f589a86513f3b",
+    "url": "/locushare/static/js/2.64343158.chunk.js"
+  },
+  {
+    "revision": "cbe7c382a6a2b6f98e28",
+    "url": "/locushare/static/js/main.86ee938c.chunk.js"
+  },
+  {
+    "revision": "086e579e0cffe2663a3a",
+    "url": "/locushare/static/js/runtime-main.cf8be614.js"
+  }
+]);
